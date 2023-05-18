56cf57344c1a84c81cb1a33d0076201c9af2b837
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-05-18 18:50:47 +0300

    Fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,30 @@
-#!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
-
-
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+    # Number of bytes in the current character
+    num_bytes = 0
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
+    # Iterate through each byte in the data
+    for byte in data:
+        # Check if it's a continuation byte (starts with '10')
+        if byte & 0b11000000 == 0b10000000:
+            # If it's not a valid continuation byte, return False
+            if num_bytes == 0:
+                return False
+            num_bytes -= 1
+        else:
+            # Check the number of bytes in the current character
+            if num_bytes > 0:
+                return False
+            # Determine the number of bytes based on the first byte
+            if byte & 0b10000000 == 0:
+                num_bytes = 0
+            elif byte & 0b11100000 == 0b11000000:
+                num_bytes = 1
+            elif byte & 0b11110000 == 0b11100000:
+                num_bytes = 2
+            elif byte & 0b11111000 == 0b11110000:
+                num_bytes = 3
+            else:
+                return False
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
-    return True
+    # If there are remaining bytes to complete a character, return False
+    return num_bytes == 0
